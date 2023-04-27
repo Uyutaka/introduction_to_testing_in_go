a7c98cd1a1664dde784be3aaf630562da037a50e
uyutaka <yutaka.urakami@colorado.edu>
2023-04-27 23:29:17 +0000

    S3 Simple Testing - 12.Improving our test with table tests

--- primeapp/main_test.go
+++ primeapp/main_test.go
@@ -2,21 +2,29 @@ package main
 
 import "testing"
 
-func Test_isPrime(t *testing.T) {
-	result, msg := isPrime(0)
-	if result {
-		t.Errorf("with %d as test parameter, got true, but expected false", 0)
+func Test_isPrime(t *testing.T){
+	primeTest:=[]struct{
+		name string
+		testNum int
+		expected bool
+		msg string
+	}{
+		{"prime", 7, true, "7 is a prime number!"},
+		{"0", 0, false, "0 is not prime, by definition!"},
+		{"negative number", -1, false, "Negative numbers are not prime, by definition!"},
+		{"not prime", 4, false, "4 is not a prime number because it is divisible by 2"},
 	}
 
-	if msg != "0 is not prime, by definition!" {
-		t.Errorf("wrong message returned, got %s", msg)
-	}
-
-	result, msg = isPrime(7)
-	if !result {
-		t.Errorf("with %d as test parameter, got false, but expected true", 7)
-	}
-	if msg != "7 is a prime number!" {
-		t.Errorf("wrong message returned, got %s", msg)
+	for _, e := range primeTest{
+		result, msg:=isPrime(e.testNum)
+		if e.expected && !result{
+			t.Errorf("%s: expected true but got false", e.name)
+		}
+		if !e.expected && result{
+			t.Errorf("%s: expected false but got true", e.name)
+		}
+		if msg != e.msg{
+			t.Errorf("%s: expected %s but got %s", e.name, e.msg, msg)
+		}
 	}
 }
