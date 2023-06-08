f2ab7c1684a8d8eeb55f1465fca8117de867f7e5
uyutaka <yutaka.urakami@colorado.edu>
2023-06-08 10:32:51 +0900

    S9 The Repository Pattern - 68.Updating our tests to use the testdb repository

--- webapp/pkg/repository/dbrepo/users_testdb.go
+++ webapp/pkg/repository/dbrepo/users_testdb.go
@@ -2,11 +2,13 @@ package dbrepo
 
 import (
 	"database/sql"
+	"errors"
+	"time"
 
 	"github.com/uyutaka/introduction_to_testing_in_go/webapp/pkg/data"
 )
 
-type TestDBRepo struct {}
+type TestDBRepo struct{}
 
 func (m *TestDBRepo) Connection() *sql.DB {
 	return nil
@@ -30,11 +32,20 @@ func (m *TestDBRepo) GetUser(id int) (*data.User, error) {
 
 // GetUserByEmail returns one user by email address
 func (m *TestDBRepo) GetUserByEmail(email string) (*data.User, error) {
-	var user = data.User{
-		ID: 1,
+	if email == "admin@example.com" {
+		user := data.User{
+			ID:        1,
+			FirstName: "Admin",
+			LastName:  "User",
+			Email:     "admin@example.com",
+			Password:  "$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK",
+			IsAdmin:   1,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		}
+		return &user, nil
 	}
-
-	return &user, nil
+	return nil, errors.New("not found")
 }
 
 // UpdateUser updates one user in the database
